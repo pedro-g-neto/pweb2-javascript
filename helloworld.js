60144a900c3d0bfa398ea9a5408379ff3afe3b32
Pedro Gomes <pedrogomesipubi2006@hotmail.com>
2026-08-17 11:18:29 -0300

    first commit

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!"); /*Printa no terminal/Console*/
+/*alert("Hello, World!"); Printa na tela do navegador */
+console.warn("Hello, World!"); /* Aviso/Warning */
+console.error("Hello, World!"); /* Error */
